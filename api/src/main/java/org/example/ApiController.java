2014d33bbbb742465c66590b953bfd5555c9f36b
김민식 <kmss69052@naver.com>
2024-08-08 15:16:08 +0900

    edit api controller

--- api/src/main/java/org/example/ApiController.java
+++ api/src/main/java/org/example/ApiController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiController {
 
-  public ApiController() {
-  }
-
   @GetMapping("/hello")
   public String getCoreVersion() {
     return "core module version id is %s".formatted(Version.id);
