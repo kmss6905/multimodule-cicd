4a96d3f6fbb1420dab042cc6d665d0d1a927e63e
김민식 <kmss69052@naver.com>
2024-08-09 17:46:52 +0900

    Edit api

--- api/src/main/java/org/example/ApiApplication.java
+++ api/src/main/java/org/example/ApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
   public static void main(String[] args) {
-    System.out.println("s");
     SpringApplication.run(ApiApplication.class, args);
   }
 }
